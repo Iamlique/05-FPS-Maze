744e0522850f2feeadab887d8e4ba92624df9cd5
Iamlique <mablyth@iu.edu>
2022-11-22 02:12:07 -0500

    Completes Project

---
+++ UI/Main.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_Play_pressed():
+	var _scene = get_tree().change_scene("res://Game.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+func _on_NextLevel_pressed():
+	var _scene = get_tree().change_scene("res://Game2.tscn")
